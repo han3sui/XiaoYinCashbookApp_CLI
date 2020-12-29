7a166a10915ca1cf3f064ccd207e52c60f231d14
Han <han3sui@gamil.com>
2020-12-29 16:51:54 +0800

    优化

---
+++ src/components/BasePullScroll.vue
@@ -0,0 +1,39 @@
+<template>
+  <movable-area>
+    <movable-view>
+      <scroll-view scroll-y :style="[customScrollStyle]" @scrolltolower="scrolltolower">
+        <view class="scroll-refresh"></view>
+        <slot/>
+      </scroll-view>
+    </movable-view>
+  </movable-area>
+</template>
+
+<script>
+export default {
+  name: 'BasePullScroll',
+  props: {
+    scrollHeight: {
+      type: [String, Number],
+      default: 90
+    }
+  },
+  computed: {
+    // Scroll内联样式
+    customScrollStyle () {
+      return {
+        height: `${this.height}vh`
+      }
+    }
+  },
+  methods: {
+    scrolltolower () {
+      this.$emit('loadmore')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
