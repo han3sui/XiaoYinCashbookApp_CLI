3f64b35c39f374be37419055f81a5ffc65dd39d1
han3sui <han3sui@gmail.com>
2021-07-24 22:39:24 +0800

    fix：更新版本号

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -38,7 +38,7 @@
       </view>
       <view class="cell-wrap">
         <base-cell>
-          <base-cell-item title='当前版本' icon="banben" note="v1.1.2"/>
+          <base-cell-item title='当前版本' icon="banben" note="v1.1.3"/>
         </base-cell>
       </view>
     </view>
