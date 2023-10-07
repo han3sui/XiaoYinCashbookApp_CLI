7cb2d50c262fb4bbe967200e40421fed8f546360
han3sui <han3sui@gmail.com>
2023-10-07 20:55:23 +0800

    fix: 调整icon获取

--- src/apis/icon.js
+++ src/apis/icon.js
@@ -1,6 +1,6 @@
 import request from "../utils/request.js";
 import config from "@/utils/config";
-const url = `${config.API_VERSION}/icons`;
+const url = `${config.API_VERSION}/common/cos/icon`;
 
 /**
  * 图标列表
